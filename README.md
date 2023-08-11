c3228d015f546945214d59e98f37a132c59e713d
KingKyoTrue <tanadicoding@gmail.com>
2023-08-12 01:01:15 +0700

    update link

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+# Live Code on Vercel
+
+Check the [code](https://login-page-xi-ochre.vercel.app/)
+
+# Source Code on GitHub
+
+Check the [code](https://github.com/lecodingdev/login-page)
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
   return (
     <div className="flex flex-col md:flex-row justify-between items-center text-center my-5 mx-8">
         <p>Build by <a href="https://github.com/lecodingdev" target='_blank' rel="noreferrer" className='border-b-2 border-black  hover:bg-black hover:text-white'>Alan Tanadi</a></p>
-        <p>The source code is available on <a href="https://github.com/lecodingdev" target='_blank' rel="noreferrer" className='border-b-2 border-black hover:bg-black hover:text-white'>GitHub</a></p>
+        <p>The source code is available on <a href="https://github.com/lecodingdev/login-page" target='_blank' rel="noreferrer" className='border-b-2 border-black hover:bg-black hover:text-white'>GitHub</a></p>
     </div>
   )
 }
